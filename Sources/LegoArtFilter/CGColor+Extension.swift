ed257ebd4bb385a0e7bd4f81b897a640ccf7b33c
Takuto NAKAMURA (Kyome) <kyomesuke@gmail.com>
2021-10-14 22:06:34 +0900

    CGColorのインタフェースを強化

---
+++ Sources/LegoArtFilter/CGColor+Extension.swift
@@ -0,0 +1,20 @@
+//
+//  CGColor+Extension.swift
+//  
+//
+//  Created by Takuto Nakamura on 2021/10/14.
+//
+
+import CoreGraphics
+import LegoColors
+
+extension CGColor {
+    func blended(_ color: CGColor, fraction: CGFloat, alpha: CGFloat) -> CGColor? {
+        guard let rgba1 = self.rgba, let rgba2 = color.rgba else { return nil }
+        let fi: CGFloat = 1 - fraction
+        return CGColor(red:   fi * rgba1.r + fraction * rgba2.r,
+                       green: fi * rgba1.g + fraction * rgba2.g,
+                       blue:  fi * rgba1.b + fraction * rgba2.b,
+                       alpha: alpha)
+    }
+}
--- Sources/LegoArtFilter/LegoArtFilter.swift
+++ Sources/LegoArtFilter/LegoArtFilter.swift
@@ -14,6 +14,8 @@ import UIKit
 import AppKit
 #endif
 
+public let CGBlack = CGColor(gray: 1.0, alpha: 1.0)
+
 public class LegoArt {
     let studType: StudType
     let baseColor: CGColor
@@ -24,7 +26,7 @@ public class LegoArt {
     
     public init?(
         ciImage: CIImage,
-        baseColor: CGColor = .black,
+        baseColor: CGColor = CGBlack,
         studType: StudType = .round,
         maxStud: Int = 48,
         studPixelWidth: Int = 10
@@ -45,11 +47,12 @@ public class LegoArt {
     #if canImport(UIKit)
     public convenience init?(
         from uiImage: UIImage,
-        baseColor: CGColor = .black,
+        baseColor: CGColor = CGBlack,
         studType: StudType = .round,
         maxStud: Int = 48,
         studPixelWidth: Int = 10
     ) {
+        // Correcting the image orientation
         let correctUIImage = UIGraphicsImageRenderer(size: uiImage.size)
             .image(actions: { _ in
                 uiImage.draw(at: .zero)
@@ -62,9 +65,7 @@ public class LegoArt {
                   maxStud: maxStud,
                   studPixelWidth: studPixelWidth)
     }
-    #endif
-    
-    #if canImport(AppKit)
+    #elseif canImport(AppKit)
     public convenience init?(
         from nsImage: NSImage,
         baseColor: CGColor = .black,
@@ -94,7 +95,7 @@ public class LegoArt {
         let size = resizedImage.extent.size
         let colorMap = (0 ..< Int(size.width * size.height))
             .map { i -> LegoColor in
-                let r = CGFloat(rgbaData[4 * i]) / 255.0
+                let r = CGFloat(rgbaData[4 * i + 0]) / 255.0
                 let g = CGFloat(rgbaData[4 * i + 1]) / 255.0
                 let b = CGFloat(rgbaData[4 * i + 2]) / 255.0
                 let a = CGFloat(rgbaData[4 * i + 3]) / 255.0
@@ -103,100 +104,65 @@ public class LegoArt {
         return (colorMap, Int(size.width))
     }
     
-    #if canImport(UIKit)
-    public func exportUIImage() -> UIImage? {
+    public func exportCGImage() -> CGImage? {
         if colorMap.isEmpty { return nil }
         let w = horizontalStudCount
         let h = colorMap.count / horizontalStudCount
         let size = CGSize(width: (studPixelWidth + 1) * w - 1,
                           height: (studPixelWidth + 1) * h - 1)
+        let rect = CGRect(origin: .zero, size: size)
         
-        return UIGraphicsImageRenderer(size: size).image { _ in
-//            let bgRect = UIBezierPath(rect: CGRect(origin: .zero, size: size))
-//            UIColor.black.setFill()
-//            bgRect.fill()
-            
-            let u = CGFloat(studPixelWidth)
-            for i in (0 ..< colorMap.count) {
-                let x = CGFloat(i % w) * (u + 1)
-                let y = CGFloat(i / w) * (u + 1)
-                
-                switch studType {
-                case .round, .roundPlate:
-                    let path = UIBezierPath(ovalIn: CGRect(x: x, y: y, width: u, height: u))
-                    UIColor(colorMap[i].color).setFill()
-                    path.fill()
-                case .square, .squarePlate:
-                    let path = UIBezierPath(rect: CGRect(x: x, y: y, width: u, height: u))
-                    UIColor(colorMap[i].color).setFill()
-                    path.fill()
-                }
-                if studType == .round || studType == .square {
-                    let v = 0.64 * u
-                    let r = 0.5 * (u - v)
-                    let rect = CGRect(x: CGFloat(x) + r, y: CGFloat(y) + r, width: v, height: v)
-                    let path = UIBezierPath(ovalIn: rect)
-                    let blendedColor = UIColor(colorMap[i].color)
-                        .blended(withFraction: 0.2, of: UIColor.black)
-                    if colorMap[i] == .transClear {
-                        blendedColor.withAlphaComponent(0.1).setFill()
-                    } else {
-                        blendedColor.withAlphaComponent(0.6).setFill()
-                    }
-                    path.fill()
-                }
-            }
-        }
-    }
-    #endif
-    
-    #if canImport(AppKit)
-    public func exportNSImage() -> NSImage? {
-        if colorMap.isEmpty { return nil }
-        let w = horizontalStudCount
-        let h = colorMap.count / horizontalStudCount
-        let size = NSSize(width: (studPixelWidth + 1) * w - 1,
-                          height: (studPixelWidth + 1) * h - 1)
-        let image = NSImage(size: size)
-        image.lockFocus()
-//        let bgRect = NSBezierPath(rect: NSRect(origin: .zero, size: size))
-//        NSColor.black.setFill()
-//        bgRect.fill()
+        guard let cgContext = CGContext(data: nil,
+                                        width: Int(size.width),
+                                        height: Int(size.height),
+                                        bitsPerComponent: 8,
+                                        bytesPerRow: 4 * Int(size.width),
+                                        space: CGColorSpaceCreateDeviceRGB(),
+                                        bitmapInfo: CGImageAlphaInfo.premultipliedLast.rawValue)
+        else { return nil }
+        // fill black
+        cgContext.setFillColor(CGBlack)
+        cgContext.fill(rect)
         
-        let q = colorMap.count / w - 1
+        // draw studs
         let u = CGFloat(studPixelWidth)
         for i in (0 ..< colorMap.count) {
             let x = CGFloat(i % w) * (u + 1)
-            let y = CGFloat(q - i / w) * (u + 1)
-            
+            let y = CGFloat(i / w) * (u + 1)
+            let c = colorMap[i].cgColor
             switch studType {
             case .round, .roundPlate:
-                let path = NSBezierPath(ovalIn: NSRect(x: x, y: y, width: u, height: u))
-                NSColor(colorMap[i].color).setFill()
-                path.fill()
+                cgContext.setFillColor(c)
+                cgContext.fillEllipse(in: CGRect(x: x, y: y, width: u, height: u))
             case .square, .squarePlate:
-                let path = NSBezierPath(rect: NSRect(x: x, y: y, width: u, height: u))
-                NSColor(colorMap[i].color).setFill()
-                path.fill()
+                cgContext.setFillColor(c)
+                cgContext.fill(CGRect(x: x, y: y, width: u, height: u))
             }
             if studType == .round || studType == .square {
-                let v = 0.64 * u
-                let r = 0.5 * (u - v)
-                let rect = NSRect(x: CGFloat(x) + r, y: CGFloat(y) + r, width: v, height: v)
-                let path = NSBezierPath(ovalIn: rect)
-                if let blendedColor = NSColor(colorMap[i].color)
-                    .blended(withFraction: 0.2, of: NSColor.black) {
-                    if colorMap[i] == .transClear {
-                        blendedColor.withAlphaComponent(0.1).setFill()
-                    } else {
-                        blendedColor.withAlphaComponent(0.6).setFill()
-                    }
+                let alpha: CGFloat = (colorMap[i] == .transClear) ? 0.1 : 0.6
+                if let bc = c.blended(CGBlack, fraction: 0.2, alpha: alpha) {
+                    cgContext.setFillColor(bc)
+                    let v = 0.64 * u
+                    let r = 0.5 * (u - v)
+                    let studRect = CGRect(x: CGFloat(x) + r, y: CGFloat(y) + r, width: v, height: v)
+                    cgContext.fillEllipse(in: studRect)
                 }
-                path.fill()
             }
         }
-        image.unlockFocus()
-        return image
+
+        return cgContext.makeImage()
+    }
+    
+    #if canImport(UIKit)
+    public func exportUIImage() -> UIImage? {
+        guard let cgImage = exportCGImage() else { return nil }
+        return UIImage(cgImage: cgImage)
+    }
+    #elseif canImport(AppKit)
+    public func exportNSImage() -> NSImage? {
+        guard let cgImage = exportCGImage() else { return nil }
+        let size = CGSize(width: cgImage.width, height: cgImage.height)
+        return NSImage(cgImage: cgImage, size: size)
     }
     #endif
 }
