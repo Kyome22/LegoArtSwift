0be93e43fafe9a2ac366736d5395a871f1261d0b
Takuto NAKAMURA (Kyome) <kyomesuke@gmail.com>
2021-10-14 17:48:14 +0900

    CGImageを正確に切り取ってピクセルを取得する

--- Sources/LegoArtFilter/CIImage+Extension.swift
+++ Sources/LegoArtFilter/CIImage+Extension.swift
@@ -15,4 +15,28 @@ extension CIImage {
         let transform = CGAffineTransform(scaleX: scale, y: scale)
         return self.transformed(by: transform)
     }
+    
+    func rgbaData(baseColor: CGColor) -> [UInt8]? {
+        let size = self.extent.size
+        let rect = CGRect(origin: .zero, size: size)
+        guard let cgContext = CGContext(data: nil,
+                                        width: Int(size.width),
+                                        height: Int(size.height),
+                                        bitsPerComponent: 8,
+                                        bytesPerRow: 4 * Int(size.width),
+                                        space: CGColorSpaceCreateDeviceRGB(),
+                                        bitmapInfo: CGImageAlphaInfo.premultipliedLast.rawValue),
+                let originalCGImage = CIContext(options: nil).createCGImage(self, from: rect)
+        else { return nil }
+        cgContext.setFillColor(baseColor)
+        cgContext.fill(rect)
+        cgContext.draw(originalCGImage, in: rect)
+        guard let correctCGImage = cgContext.makeImage(),
+              let data = correctCGImage.dataProvider?.data
+        else { return nil }
+        let length = CFDataGetLength(data)
+        var rgbaData = [UInt8](repeating: 0, count: length)
+        CFDataGetBytes(data, CFRange(location: 0, length: length), &rgbaData)
+        return rgbaData
+    }
 }
--- Sources/LegoArtFilter/LegoArtFilter.swift
+++ Sources/LegoArtFilter/LegoArtFilter.swift
@@ -16,21 +16,25 @@ import AppKit
 
 public class LegoArt {
     let studType: StudType
+    let baseColor: CGColor
     let maxStud: Int
     let studPixelWidth: Int
     let horizontalStudCount: Int
     let colorMap: [LegoColor]
     
-    // 透明度のことは考えていない
     public init?(
         ciImage: CIImage,
+        baseColor: CGColor = .black,
         studType: StudType = .round,
         maxStud: Int = 48,
         studPixelWidth: Int = 10
     ) {
         if maxStud <= 0 { return nil }
-        let (colorMap, count) = LegoArt.getColorMap(from: ciImage, maxStud: maxStud)
+        let (colorMap, count) = LegoArt.getColorMap(from: ciImage,
+                                                    baseColor: baseColor,
+                                                    maxStud: maxStud)
         if colorMap.isEmpty { return nil }
+        self.baseColor = baseColor
         self.studType = studType
         self.maxStud = maxStud
         self.studPixelWidth = studPixelWidth
@@ -41,6 +45,7 @@ public class LegoArt {
     #if canImport(UIKit)
     public convenience init?(
         from uiImage: UIImage,
+        baseColor: CGColor = .black,
         studType: StudType = .round,
         maxStud: Int = 48,
         studPixelWidth: Int = 10
@@ -52,6 +57,7 @@ public class LegoArt {
         guard let cgImage = correctUIImage.cgImage else { return nil }
         let ciImage = CIImage(cgImage: cgImage)
         self.init(ciImage: ciImage,
+                  baseColor: baseColor,
                   studType: studType,
                   maxStud: maxStud,
                   studPixelWidth: studPixelWidth)
@@ -61,6 +67,7 @@ public class LegoArt {
     #if canImport(AppKit)
     public convenience init?(
         from nsImage: NSImage,
+        baseColor: CGColor = .black,
         studType: StudType = .round,
         maxStud: Int = 48,
         studPixelWidth: Int = 10
@@ -69,25 +76,28 @@ public class LegoArt {
               let ciImage = CIImage(data: imageData)
         else { return nil }
         self.init(ciImage: ciImage,
+                  baseColor: baseColor,
                   studType: studType,
                   maxStud: maxStud,
                   studPixelWidth: studPixelWidth)
     }
     #endif
     
-    private static func getColorMap(from ciImage: CIImage, maxStud: Int) -> ([LegoColor], Int) {
-        let context = CIContext(options: nil)
+    private static func getColorMap(
+        from ciImage: CIImage,
+        baseColor: CGColor,
+        maxStud: Int
+    ) -> ([LegoColor], Int) {
         guard let resizedImage = ciImage.resizeAffine(maxStud: maxStud),
-              let cgImage = context.createCGImage(resizedImage, from: resizedImage.extent),
-              let pixelData = cgImage.rgbData
+              let rgbaData = resizedImage.rgbaData(baseColor: baseColor)
         else { return ([], 0) }
         let size = resizedImage.extent.size
         let colorMap = (0 ..< Int(size.width * size.height))
             .map { i -> LegoColor in
-                let r = CGFloat(pixelData[4 * i]) / 255.0
-                let g = CGFloat(pixelData[4 * i + 1]) / 255.0
-                let b = CGFloat(pixelData[4 * i + 2]) / 255.0
-                let a = CGFloat(pixelData[4 * i + 3]) / 255.0
+                let r = CGFloat(rgbaData[4 * i]) / 255.0
+                let g = CGFloat(rgbaData[4 * i + 1]) / 255.0
+                let b = CGFloat(rgbaData[4 * i + 2]) / 255.0
+                let a = CGFloat(rgbaData[4 * i + 3]) / 255.0
                 return LegoColor(r: r, g: g, b: b, a: a)
             }
         return (colorMap, Int(size.width))
@@ -102,9 +112,9 @@ public class LegoArt {
                           height: (studPixelWidth + 1) * h - 1)
         
         return UIGraphicsImageRenderer(size: size).image { _ in
-            let bgRect = UIBezierPath(rect: CGRect(origin: .zero, size: size))
-            UIColor.black.setFill()
-            bgRect.fill()
+//            let bgRect = UIBezierPath(rect: CGRect(origin: .zero, size: size))
+//            UIColor.black.setFill()
+//            bgRect.fill()
             
             let u = CGFloat(studPixelWidth)
             for i in (0 ..< colorMap.count) {
@@ -149,9 +159,9 @@ public class LegoArt {
                           height: (studPixelWidth + 1) * h - 1)
         let image = NSImage(size: size)
         image.lockFocus()
-        let bgRect = NSBezierPath(rect: NSRect(origin: .zero, size: size))
-        NSColor.black.setFill()
-        bgRect.fill()
+//        let bgRect = NSBezierPath(rect: NSRect(origin: .zero, size: size))
+//        NSColor.black.setFill()
+//        bgRect.fill()
         
         let q = colorMap.count / w - 1
         let u = CGFloat(studPixelWidth)
